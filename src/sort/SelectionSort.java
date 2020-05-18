41069c1af7896b455aa4a8dc5046a2b8ff911a29
Kira <datdhvinh@gmail.com>
2020-05-18 14:33:43 +0700

    Cài đặt thuật toán sắp xếp chọn

---
+++ src/sort/SelectionSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+public class SelectionSort {
+    public static void selectionSort(int[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            int currentMinValue = list[i];
+            int currentMinIndex = i;
+            for (int j = 1; j < list.length; j++) {
+                if (currentMinValue > list[j]) {
+                    currentMinValue = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMinValue;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] list = {6, 3, 8, 4, 9, 1, -3};
+        selectionSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+    }
+}
